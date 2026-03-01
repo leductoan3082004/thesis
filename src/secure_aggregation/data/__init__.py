abf05a8a327430b0a6dd3b62ec87f63ac44f4624
thanhle364 <letienthanh364@gmail.com>
2026-03-01 19:14:29 +0700

    fix import problem

--- src/secure_aggregation/data/__init__.py
+++ src/secure_aggregation/data/__init__.py
@@ -1,6 +1,9 @@
+from .dataset import get_labels, load_dataset
 from .partition import dirichlet_partition, iid_partition
 
 __all__ = [
     "dirichlet_partition",
     "iid_partition",
+    "get_labels",
+    "load_dataset",
 ]
